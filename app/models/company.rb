aeed95c1bcde3d56a6dd027e815f4bbeadac9767
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-12 06:25:52 +0300

    Created new freebie, return oldest_company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,15 @@
 class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
+
+    # give_freebie
+    def give_freebie(dev, item_name, value)
+        freebie = Freebie.new(item_name: item_name, value: value, dev_id: dev.id, company_id: self.id)
+        freebie.save
+    end
+
+    # oldest company
+    def self.oldest_company
+        order(:founding_year).first
+      end
 end
