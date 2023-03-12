6dfdddb588839cab8502aa9ba5dd68d239a409f3
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-12 06:07:45 +0300

    Print details

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,4 +1,10 @@
 class Freebie < ActiveRecord::Base
     belongs_to :dev
     belongs_to :company
+
+    # print_details
+    # {insert dev's name} owns a {insert freebie's item_name} from {insert company's name}
+    def print_details
+        puts "#{self.dev.name} owns a #{self.freebie.item_name} from #{self.company.name}"
+    end
 end
